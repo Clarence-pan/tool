49f0134c53c6eec02fab0cbc409a95cfcead5d7d
panchangyun <panchangyun@tuniu.com>
2014-12-30 11:47:57 +0800

    adjust styles

--- protected/util/view/markdown-to-html.php
+++ protected/util/view/markdown-to-html.php
@@ -1,3 +1,43 @@
 <article>
 <?php echo $html; ?>
 </article>
+
+<!-- the following is style for this article -->
+<style>
+    td, th {
+        padding: 0.2em;
+        border: 1px solid #777;
+    }
+    p {
+        white-space: pre;
+    }
+    .content{
+        font-size: 10px;
+        line-height: 12px;
+        margin-left: 10em;
+    }
+</style>
+<script>
+    $(function(){
+        function makeContent(){
+            var id = 1;
+            var $content = $('<div class="content"></div>');
+            $("h1,h2,h3,h4,h5,h6").each(function(){
+                var $this = $(this);
+                var $contentPointer = $this.clone().removeAttr('id');
+                var $thisId = $this.attr('id');
+                if (!$thisId){
+                    $thisId = 'content-'+id;
+                    id++;
+                    $this.attr('id', $thisId);
+                }
+                $contentPointer.wrapInner('<a href="#'+$thisId+'" ></a>');
+                $content.append($contentPointer);
+            })
+            $content.prependTo('body');
+        }
+        makeContent();
+    });
+</script>
+
+

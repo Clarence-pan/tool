41678f17bb77ae3c99ba809dcd9bfcb904fca479
panchangyun <panchangyun@tuniu.com>
2014-12-30 12:52:41 +0800

    alter styles

--- protected/util/view/markdown-to-html.php
+++ protected/util/view/markdown-to-html.php
@@ -16,6 +16,32 @@
         line-height: 12px;
         margin-left: 10em;
     }
+    h1:before,h2:before,h3:before,h4:before,h5:before{
+        content: '* ';
+    }
+    h1,h2,h3,h4,h5{
+        font-weight: bold;
+    }
+    h1{
+        font-size: 20px;
+        padding-left: 0;
+    }
+    h2{
+        font-size: 17px;
+        padding-left: 1em;
+    }
+    h3{
+        font-size: 15px;
+        padding-left: 2em;
+    }
+    h4{
+        font-size: 13px;
+        padding-left: 3em;
+    }
+    h5{
+        font-size: 12px;
+        padding-left: 4em;
+    }
 </style>
 <script>
     $(function(){

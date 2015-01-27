116dc1d32bfd610bcb00d211fbb106c3002d8c99
panchangyun <panchangyun@tuniu.com>
2015-01-27 17:33:40 +0800

    add interested logs view all

--- protected/util/view/log/_html_header.php
+++ protected/util/view/log/_html_header.php
@@ -5,11 +5,11 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <script type='text/javascript' src='/static/js/jquery.js'></script>
     <script type="text/javascript">
-        <?php echo file_get_contents(__DIR__.'/log/log.js') ?>
+        <?php echo file_get_contents(__DIR__.'/log.js') ?>
     </script>
     <title></title>
     <style type="text/css" >
-        <?php echo file_get_contents(__DIR__.'/log/log.css') ?>
+        <?php echo file_get_contents(__DIR__.'/log.css') ?>
         li.stackTrace{
         <?PHP if (!@$_REQUEST['displayStackTrace']): ?>
             display: none ;

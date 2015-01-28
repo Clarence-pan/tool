be559c3d7a11fbf204fdf3748ddfb19a634f67b6
panchangyun <panchangyun@tuniu.com>
2015-01-28 11:21:22 +0800

    the last mine tool

---
+++ protected/base/Object.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace tool\base;
+
+
+class Object {
+    public function __call($method, $args){
+
+    }
+    public function __staticCall($method, $args){
+
+    }
+    public function __set($field, $value){
+
+    }
+    public function __get($field, $value){
+
+    }
+} 

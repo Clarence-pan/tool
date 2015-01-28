e2ef8e876d530531878a77ceef5064e55efaec00
panchangyun <panchangyun@tuniu.com>
2015-01-28 10:29:34 +0800

    publish

--- protected/util/view/log/log.js
+++ protected/util/view/log/log.js
@@ -112,10 +112,115 @@ function showInNewWindow(e){
     if (e.innerHTML.split("\n").length > 3){
         var x = window.open('', '_blank');
         x.document.body.innerHTML = '<pre>'+e.innerHTML+'</pre>';
+        bindExpander($(x.document.body).children('pre'));
     }
 }
-function expandThisCell(e){
-    $(e).toggleClass('fixed-height');
+function bindExpander(code){
+    if (!code.text().trim().match(/array/i)){
+        return;
+    }
+//    var css = document.createElement('style');
+//    css.innerHTML = '.expander-open,.expander-close{ color: blue; }';
+//    code.closest('document')[0].head.prependChild(css);
+
+	(function(){
+		var lines = code.text().split('\n');
+		code.empty();
+		for (var i = 0; i < lines.length; i++){
+			var line = lines[i];
+			var intendClass = 'intend-'+findFirstNonBlankPos(line);
+			var line = $('<div class="line"></div>').text(line).attr('data-intend-class', intendClass).addClass(intendClass);
+			line.html(line.html().replace('(', '<span class="expander-open">(</span>').replace(')', '<span class="expander-close">)</span>').replace('array', '<span class="expander-array">array</span>'));
+			code.append(line);
+		}
+	})();
+	var expanderCss = {
+        color: 'blue',
+        'padding-right': '2em',
+		'margin-right': '-2em',
+        'padding-left': '2em',
+        'margin-left': '-2em',
+        cursor: 'pointer'
+    };
+    code.find('.expander-open, .expander-close, .expander-array').css(expanderCss);
+	code.delegate('.expander-array', 'click', function(){
+		var opener = $(this).closest('.line').next().find('.expander-open:eq(0)');
+		if (opener.is(':visible')){
+			opener.trigger('click');
+		}else{
+			$(this).next().trigger('click');
+		}
+	});
+	code.delegate('.expander-close', 'click', function(){
+		var line = $(this).closest('.line');
+		var opener = findNextDired(line, 'prev', '.' + line.attr('data-intend-class')).find('.expander-open:eq(0)');
+		opener.trigger('click');
+	});
+    code.delegate('.expander-open', 'click', function(){
+        var $this = $(this);
+        var line = $this.closest('.line');
+		var prevLine = findNextDired(line, 'prev', '.line');
+        var thisClass = '.' + line.attr('data-intend-class');
+        var content = line.nextUntil(thisClass);
+        var endPos = findNextDired(line, 'next', thisClass);
+		if (!line.data('wrapper')){
+			var wrapper = $('<div class="wrapper"></div>').insertBefore(line);
+			wrapper.append(line).append(content).append(endPos);
+			line.data('wrapper', wrapper);
+		}else{
+			var wrapper = line.data('wrapper');
+			prevLine = wrapper.prev();
+			if (prevLine.find('.expander').size() > 0){
+				return;
+			}
+		}
+		wrapper.hide();
+
+		var expander = $('<span class="expander">(...) <i>// </i></span>');
+		expander.appendTo(prevLine);
+		expander.append($('<i></i>').text(content.text().substring(0, 90)+'...'));
+		expander.css(expanderCss).css({
+			'white-space': 'initial'
+		});
+		expander.on('click', function(){
+			wrapper.show();
+			expander.remove();
+		});
+    });
+	$('<div></div>').append(
+			$('<a href="javascript:void(0)">Close All</a>').on('click', function(){
+				code.find('.expander-open').trigger('click');
+			})).append(
+			$('<a href="javascript:void(0)">Open All</a>').on('click', function(){
+				code.find('.expander').trigger('click');
+			})
+		).prependTo(code).find('a').css({
+			'margin-left' : '2em'
+		});;
+
+	function findNextDired(elem, dir, selector){
+		for (var next = elem[dir](); !next.is(selector); next = next[dir]()){
+			if (next.size() <= 0){
+				break;
+			}
+		}
+		return next;
+	}
+    function findFirstNonBlankPos(str){
+        for (var j = 0; j < str.length; j++){
+            if (str[j] != ' '){
+                return j;
+            }
+        }
+        return false;
+    }
+}
+function expandThisCell(e, evt){
+    var top = $(e).toggleClass('fixed-height').offset().top;
+    // 如果滚动得超过窗口范围了，自动滚回去
+    if (window.scrollY > top ){
+        window.scrollTo(0, top - evt.clientY);
+    }
 }
 function toggle_very_brief(v){
     $('.line, .level, .category, .time').toggle(!v);

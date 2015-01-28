ad4ce77948b57120ae65934b3bf41d6171dd0f61
panchangyun <panchangyun@tuniu.com>
2015-01-28 17:56:21 +0800

    remove debug

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 // remove the following lines when in production mode
+//$_REQUEST['debug'] = 1;
 defined('YII_DEBUG') or define('YII_DEBUG', !!$_REQUEST['debug']);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);

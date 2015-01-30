3a5c266ef7943e3a7fddb2a7677943e64b04ced6
panchangyun <panchangyun@tuniu.com>
2015-01-30 14:17:11 +0800

    add pager

--- protected/modules/log/views/default/pager.php
+++ protected/modules/log/views/default/pager.php
@@ -9,20 +9,20 @@ $pageCount = $count / $limit; // 页数
 ?>
 <div style="width: 100%; text-align: right">
 <?php
-    echo CHtml::link('<<', array('', 'start' => 0, 'limit' => $limit));
-    if ($start > 0){
-        echo CHtml::link(' < ', array('', 'start' => max($start - $limit, 0), 'limit' => $limit));
-    }
-    $showPageCount = 10;
+    $showPageCount = 10; // 显示几页
     $startPage = max(0, min($currentPage - intval($showPageCount / 2), $pageCount - $showPageCount));
     $endPage = $startPage + $showPageCount;
+    if ($start > 0){
+        echo CHtml::link('<<', array('', 'start' => 0, 'limit' => $limit));
+        echo CHtml::link(' < ', array('', 'start' => max($start - $limit, 0), 'limit' => $limit));
+    }
     for ($page = $startPage; $page < $pageCount && $page < $endPage; $page++){
         echo CHtml::link(" $page ", array('', 'start' => max($page * $limit, 0), 'limit' => $limit));
     }
     if ($start + $limit < $count){
         echo CHtml::link(' > ', array('', 'start' => min($start + $limit, $count), 'limit' => $limit));
+        echo CHtml::link('>>', array('', 'start' => floor($count / $limit) * $limit, 'limit' => $limit ));
     }
-    echo CHtml::link('>>', array('', 'start' => floor($count / $limit) * $limit, 'limit' => $limit ));
     echo "[$currentPage/$pageCount(Total: $count)]";
 ?>
 </div>

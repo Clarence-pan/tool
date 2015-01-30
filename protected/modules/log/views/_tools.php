c5510a9d6148f70be0ad2c100972d91faa29e8ec
panchangyun <panchangyun@tuniu.com>
2015-01-30 14:47:41 +0800

    fix bugs

--- protected/modules/log/views/_tools.php
+++ protected/modules/log/views/_tools.php
@@ -16,7 +16,7 @@
         <input type="button" onclick="scrollToTop()" value="Top" />
         <input type="button" onclick="scrollToBottom()" value="Bottom" />
         <?php foreach ($links as $display => $href) {
-                    echo CHtml::link($display, $href, $pager);
+                    echo CHtml::link($display, array_merge($href, $pager));
                     echo "<span> </span>";
          }  ?>
         <label>

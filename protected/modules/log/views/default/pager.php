0450a912d94c47657fd6ca857c9cf3297c6b2f73
panchangyun <panchangyun@tuniu.com>
2015-01-30 14:13:22 +0800

    add pager

--- protected/modules/log/views/default/pager.php
+++ protected/modules/log/views/default/pager.php
@@ -4,6 +4,7 @@
  * @var $start int
  * @var $limit int
  */
+$currentPage = intval($start / $limit);
 $pageCount = ($count - $start);
 if ($pageCount > $limit){
     $pageCount = $limit;
@@ -14,6 +15,12 @@ if ($pageCount > $limit){
     echo CHtml::link('<<', array('', 'start' => 0, 'limit' => $limit));
     if ($start > 0){
         echo CHtml::link(' < ', array('', 'start' => max($start - $limit, 0), 'limit' => $limit));
+    }
+    $showPageCount = 10;
+    $startPage = max(0, min($currentPage - intval($showPageCount / 2), $pageCount - $showPageCount));
+    $endPage = $startPage + $showPageCount;
+    for ($page = $startPage; $page < $pageCount && $page < $endPage; $page++){
+        echo CHtml::link(" $page ", array('', 'start' => max($page * $limit, 0), 'limit' => $limit));
     }
     if ($start + $limit < $count){
         echo CHtml::link(' > ', array('', 'start' => min($start + $limit, $count), 'limit' => $limit));

0932a8646da851c378f24e2e2ec73169f057c2fb
panchangyun <panchangyun@tuniu.com>
2015-02-04 20:03:23 +0800

    summary

--- protected/modules/log/views/default/help.php
+++ protected/modules/log/views/default/help.php
@@ -8,22 +8,27 @@
         <input type="checkbox" name="sum" />
         Summary - whether display summary of requests (not perfect!)
     </label>
+    <br/>
     <label>
         <input type="checkbox" name="profile" />
         Profile - whether display profile results
     </label>
+    <br/>
     <label>
         <input type="text" name="heading" />
         matching @msgHead at index 0
     </label>
+    <br/>
     <label>
         <input type="text" name="category" />
         substring matching @category
     </label>
+    <br/>
     <label>
         <input type="text" name="request" />
         substring matching @request URL
     </label>
+    <br/>
     <label>
         Start from <input type="number" name="start" value="0" />
     </label>

13fcc15ac26d5de90106949b6d754bdf39899392
panchangyun <panchangyun@tuniu.com>
2015-04-01 10:13:50 +0800

    增加加密测试的模块

--- test/cryptographer_example.php
+++ test/cryptographer_example.php
@@ -7,7 +7,7 @@ $cryptographer = new Cryptographer("test-password", MCRYPT_RIJNDAEL_128);
 
 // 加密
 $encrypted = $cryptographer->encrypt('test-data');
-echo $encrypted.PHP_EOL;    // jS1aqgmHsJrH2n4+Iy35WuOTffeBdGdjASeuflAsPdM=
+echo $encrypted.PHP_EOL;    // jS1aqgmHsJrH2n4+Iy35WuOTffeBdGdjASeuflAsPdM=    (由于有随机生成的IV进行保护，每次加密的结果是不同的)
 
 // 解密
 $decrypted = $cryptographer->decrypt($encrypted);

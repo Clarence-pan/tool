17eb5fd9c41fcb16ba441ef0ba104ff3e9ee1b12
panchangyun <panchangyun@tuniu.com>
2015-04-02 11:18:15 +0800

    test xss

---
+++ test-xss/index.php
@@ -0,0 +1,140 @@
+<?php
+
+$docFile = __DIR__ . '/productDetail.html';
+if ($_GET['productDetail']) {
+    file_put_contents($docFile, $_GET['productDetail']);
+    unset($_GET['productDetail']);
+    header('Location: http://tool.pcy.tuniu.org/test-xss/index.php?' . http_build_query($_GET));
+    die;
+} else {
+    $_GET['productDetail'] = file_get_contents($docFile);
+}
+
+$originalDoc = $_GET['productDetail'];
+
+$_GET[$_GET['operator']] = true;
+
+if ($_GET['encode']) {
+    $encode = function ($s) {
+        return htmlspecialchars($s);
+    };
+} else {
+    $encode = function ($s) {
+        return $s;
+    };
+}
+
+if ($_GET['security']) {
+    require(__DIR__ . '/Security.php');
+    $_GET['productDetail'] = Security::getXssSafeParam($_GET['productDetail']);
+}
+
+if ($_GET['purify']) {
+    require __DIR__ . '/htmlpurifier/library/HTMLPurifier.auto.php';
+    $config = HTMLPurifier_Config::createDefault();
+    $purifier = new HTMLPurifier($config);
+    $_GET['productDetail'] = $purifier->purify($_GET['productDetail']);
+}
+
+?>
+<script type="application/javascript" src="/static/js/jquery.js"></script>
+<style>
+    .box {
+        float: left;
+
+    }
+    .output{
+        min-width: 100em;
+        border: 1px solid #000000;
+    }
+    #summary{
+        background-color: #eadede;
+        border-radius: 5px;
+        border: 1px solid #102020;
+        float: left;
+    }
+    #summary b{
+        display: inline-block;
+        width: 10em;
+        text-align: left;
+        margin-right: 2em;
+    }
+    #summary i{
+        display: inline-block;
+        font-style: normal;
+        margin-right: 2em;
+    }
+
+</style>
+<form method="get">
+
+    <div class="">
+        Operations:
+
+        <label>
+            <input type="radio" name="operator" value="none" <?php echo $_GET['none'] ? 'checked' : '' ?> />
+            None
+        </label>
+        <label>
+            <input type="radio" name="operator" value="encode" <?php echo $_GET['encode'] ? 'checked' : '' ?> />
+            Encode
+        </label>
+        <label>
+            <input type="radio" name="operator" value="security" <?php echo $_GET['security'] ? 'checked' : '' ?> />
+            Security
+        </label>
+        <label>
+            <input type="radio" name="operator" value="purify" <?php echo $_GET['purify'] ? 'checked' : '' ?> />
+            Purify
+        </label>
+        <input type="submit"/>
+        <a href="#summary">Summary</a>
+    </div>
+    <div class="box">
+        <h3>Original Input:</h3>
+        <textarea name="productDetail" cols="110" rows="10"><?php echo htmlspecialchars($originalDoc) ?></textarea>
+    </div>
+    <div class="box">
+        <h3>After <?php echo $_GET['operator'] ?>:</h3>
+        <textarea cols="110" rows="10"><?php echo $encode($_GET['productDetail']) ?></textarea>
+    </div>
+
+    <div class="box">
+        <h3>Output:</h3>
+        <div class="output">
+            <?php echo $encode($_GET['productDetail']) ?>
+        </div>
+    </div>
+</form>
+<div id="summary">
+    <ul>
+    </ul>
+</div>
+<script type="application/javascript">
+    function outputSupport(feature, isSupport){
+        $('<li></li>')
+            .append($('<b></b>').text(feature))
+            .append($('<i></i>').text(isSupport ? 'Support' : 'NOT Support'))
+            .css('color', isSupport ? 'green' : 'red')
+            .appendTo('#summary ul');
+    }
+
+    String.prototype.contains = function(search){
+        return this.indexOf(search) >= 0;
+    };
+
+    $(function(){
+        var tags = 'h1 h2 h3 h4 h5 a img b i strong iframe script style embed'.split(' ');
+        $.each(tags, function(i, tag){
+            outputSupport('<'+tag+'>', $('.output').find(tag).size() > 0);
+        });
+
+        // 测试元素属性支持情况
+        outputSupport('inline style', $('.output').find('[style]').size() > 0);
+        outputSupport('Element class', $('.output').find('[class]').size() > 0);
+        outputSupport('Element ID', $('.output').find('[id]').size() > 0);
+
+        // 测试其他支持清空
+        outputSupport('flash', $('.output').html().contains('application/x-shockwave-flash'));
+    });
+</script>
---
+++ test/xss.php
@@ -0,0 +1,86 @@
+<?php
+
+$nameFile = __DIR__.'/name.txt';
+if ($_GET['name']){
+    file_put_contents($nameFile, $_GET['name']);
+    unset($_GET['name']);
+    header('Location: http://tool.pcy.tuniu.org/test/xss.php?'.http_build_query($_GET));
+    die;
+} else {
+    $_GET['name'] = file_get_contents($nameFile);
+}
+
+if ($_GET['encode']){
+    $encode = function ($s){ return htmlspecialchars($s); };
+} else {
+    $encode = function ($s) { return $s; };
+}
+
+function RemoveXSS($val)  {
+    // remove all non-printable characters. CR(0a) and LF(0b) and TAB(9) are allowed
+    // this prevents some character re-spacing such as <java\0script>
+    // note that you have to handle splits with \n, \r, and \t later since they *are* allowed in some inputs
+    $val = preg_replace('/([\x00-\x08][\x0b-\x0c][\x0e-\x20])/', '', $val);
+//    $val = addslashes($val);//防止unicode跨站脚本攻击    modify by chendaoyan 2014-01-14
+
+    // straight replacements, the user should never need these since they're normal characters
+    // this prevents like <IMG SRC=&#X40&#X61&#X76&#X61&#X73&#X63&#X72&#X69&#X70&#X74&#X3A&#X61&#X6C&#X65&#X72&#X74&#X28&#X27&#X58&#X53&#X53&#X27&#X29>
+    $search = 'abcdefghijklmnopqrstuvwxyz';
+    $search.= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $search.= '1234567890!@#$%^&*()';
+    $search.= '~`";:?+/={}[]-_|\'\\';
+
+    for ($i = 0; $i < strlen($search); $i++) {
+        // ;? matches the ;, which is optional
+        // 0{0,7} matches any padded zeros, which are optional and go up to 8 chars
+
+        // &#x0040 @ search for the hex values
+        $val = preg_replace('/(&#[x|X]0{0,8}'.dechex(ord($search[$i])).';?)/i', $search[$i], $val); // with a ;
+        // &# @ 0{0,7} matches '0' zero to seven times
+        $val = preg_replace('/(&#0{0,8}'.ord($search[$i]).';?)/', $search[$i], $val); // with a ;
+    }
+
+    // now the only remaining whitespace attacks are \t, \n, and \r
+    $ra1 = array('alert','javascript', 'vbscript', 'expression', 'applet', 'meta', 'xml', 'blink', 'link', 'style', 'script', 'embed', 'object', 'iframe', 'frame', 'frameset', 'ilayer', 'layer', 'bgsound', 'title', 'base');
+    $ra2 = array('onabort', 'onactivate', 'onafterprint', 'onafterupdate', 'onbeforeactivate', 'onbeforecopy', 'onbeforecut', 'onbeforedeactivate', 'onbeforeeditfocus', 'onbeforepaste', 'onbeforeprint', 'onbeforeunload', 'onbeforeupdate', 'onblur', 'onbounce', 'oncellchange', 'onchange', 'onclick', 'oncontextmenu', 'oncontrolselect', 'oncopy', 'oncut', 'ondataavailable', 'ondatasetchanged', 'ondatasetcomplete', 'ondblclick', 'ondeactivate', 'ondrag', 'ondragend', 'ondragenter', 'ondragleave', 'ondragover', 'ondragstart', 'ondrop', 'onerror', 'onerrorupdate', 'onfilterchange', 'onfinish', 'onfocus', 'onfocusin', 'onfocusout', 'onhelp', 'onkeydown', 'onkeypress', 'onkeyup', 'onlayoutcomplete', 'onload', 'onlosecapture', 'onmousedown', 'onmouseenter', 'onmouseleave', 'onmousemove', 'onmouseout', 'onmouseover', 'onmouseup', 'onmousewheel', 'onmove', 'onmoveend', 'onmovestart', 'onpaste', 'onpropertychange', 'onreadystatechange', 'onreset', 'onresize', 'onresizeend', 'onresizestart', 'onrowenter', 'onrowexit', 'onrowsdelete', 'onrowsinserted', 'onscroll', 'onselect', 'onselectionchange', 'onselectstart', 'onstart', 'onstop', 'onsubmit', 'onunload', 'confirm', 'eval', 'document');
+    $ra = array_merge($ra1, $ra2);
+
+    $found = true; // keep replacing as long as the previous round replaced something
+    while ($found == true) {
+        $val_before = $val;
+        for ($i = 0; $i < sizeof($ra); $i++) {
+            $pattern = '/';
+            for ($j = 0; $j < strlen($ra[$i]); $j++) {
+                if ($j > 0) {
+                    $pattern .= '(';
+                    $pattern .= '(&#[x|X]0{0,8}([9][a][b]);?)?';
+                    $pattern .= '|(&#0{0,8}([9][10][13]);?)?';
+                    $pattern .= ')?';
+                }
+                $pattern .= $ra[$i][$j];
+            }
+            $pattern .= '/i';
+            $replacement = substr($ra[$i], 0, 2).'<x>'.substr($ra[$i], 2); // add in <> to nerf the tag
+            $val = preg_replace($pattern, $replacement, $val); // filter out the hex tags
+            if ($val_before == $val) {
+                // no replacements were made, so exit the loop
+                $found = false;
+            }
+        }
+    }
+
+    return $val;
+}
+
+$_GET['name'] = RemoveXSS($_GET['name']);
+
+ ?>
+<form method="get">
+<!--    <input type="text" name="name" style="width: 120em" value="--><?php //echo $encode($_GET['name']) ?><!--" />-->
+    <textarea name="name" cols="120" rows="10"><?php echo $encode($_GET['name']) ?></textarea>
+    <div>
+        Name: <?php echo $encode($_GET['name']) ?>
+    </div>
+    <input type="checkbox" name="encode" <?php echo $_GET['encode'] ? 'checked' : '' ?> />
+    <input type="submit" />
+</form>

7326fc87821d37eb567a080df7281a1bdeba6737
panchangyun <panchangyun@tuniu.com>
2015-06-12 17:18:36 +0800

    可选初始向量是否随机

--- protected/components/Cryptographer.php
+++ protected/components/Cryptographer.php
@@ -34,12 +34,23 @@ class Cryptographer
      * @param string $key 密码
      * @param string $algorithm 加密算法，推荐AES
      * @param string $mode 加密模式，推荐CBC，不支持stream
+     * @param bool $useRandomIv 是否使用随机的初始向量
      */
-    public function __construct($key, $algorithm = MCRYPT_RIJNDAEL_128, $mode = MCRYPT_MODE_CBC)
+    public function __construct($key, $algorithm = MCRYPT_RIJNDAEL_128, $mode = MCRYPT_MODE_CBC, $useRandomIv=true)
     {
         $this->key = $key;
         $this->algorithm = $algorithm;
         $this->mode = $mode;
+        $this->useRandomIv = $useRandomIv;
+        $this->ivSize = mcrypt_get_iv_size($this->algorithm, $this->mode);
+
+        if ($this->useRandomIv){
+            // 使用MCRYPT_RAND产生初始向量一般就足够安全了 —— 如果使用MCRYPT_DEV_RANDOM可能会更安全点，但是速度会比MCRYPT_RAND慢很多
+            $this->iv = mcrypt_create_iv($this->ivSize, MCRYPT_RAND);
+        } else {
+            // 如果不想使用随机的初始化向量则可以用密钥作为初始向量
+            $this->iv = substr(str_pad($this->key, $this->ivSize, "\0"), 0, $this->ivSize);
+        }
     }
 
     /**
@@ -49,19 +60,14 @@ class Cryptographer
      */
     public function encrypt($text)
     {
-        $ivSize = mcrypt_get_iv_size($this->algorithm, $this->mode);
-
-        // 使用MCRYPT_RAND产生初始向量一般就足够安全了 —— 如果使用MCRYPT_DEV_RANDOM可能会更安全点，但是速度会比MCRYPT_RAND慢很多
-        $iv = mcrypt_create_iv($ivSize, MCRYPT_RAND);
-
         // 加密数据
-        $encryptedData = mcrypt_encrypt($this->algorithm, $this->key, $text, $this->mode, $iv);
+        $encryptedData = mcrypt_encrypt($this->algorithm, $this->key, $text, $this->mode, $this->iv);
         if ($encryptedData === false){
             return false;
         }
 
         // 加密后的数据是二进制的数据 —— 为了便于存储，使用BASE64进行编码，转换为可见字符
-        return base64_encode($iv.$encryptedData);
+        return base64_encode($this->useRandomIv ? $this->iv . $encryptedData : $encryptedData);
     }
 
     /**
@@ -73,13 +79,17 @@ class Cryptographer
     public function decrypt($encryptedData)
     {
         $encryptedData = base64_decode($encryptedData);
-        $ivSize = mcrypt_get_iv_size($this->algorithm, $this->mode);
-        if (strlen($encryptedData) < $ivSize) {
-            throw new InvalidArgumentException('The encrypted data is too short: '.strlen($encryptedData));
+        if ($this->useRandomIv) {
+            if (strlen($encryptedData) < $this->ivSize) {
+                throw new InvalidArgumentException('The encrypted data is too short: ' . strlen($encryptedData));
+            }
+
+            $iv = substr($encryptedData, 0, $this->ivSize);
+            $encryptedData = substr($encryptedData, $this->ivSize);
+        } else {
+            $iv = $this->iv;
         }
 
-        $iv = substr($encryptedData, 0, $ivSize);
-        $encryptedData = substr($encryptedData, $ivSize);
         $text = mcrypt_decrypt($this->algorithm, $this->key, $encryptedData, $this->mode, $iv);
         return rtrim($text, "\0");
     }
@@ -98,4 +108,20 @@ class Cryptographer
      * @var string 加密模式
      */
     private $mode;
+
+    /**
+     * @var string/bin 初始向量
+     */
+    private $iv;
+
+    /**
+     * @var int 初始向量大小
+     */
+    private $ivSize;
+
+    /**
+     * @var bool 是否使用随机的初始向量
+     */
+    private $useRandomIv;
+
 }
